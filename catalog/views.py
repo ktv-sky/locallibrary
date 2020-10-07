d972e9b47500ec1774c9941f6bfac6445f7ad691
ktv-sky <helicooper7@gmail.com>
2020-10-07 11:52:51 +0300

    Add index page and templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre, Language
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a'
+        ).count()
+    num_authors = Author.objects.count() # method all() by default
+    num_genres = Genre.objects.count()
+    num_languages = Language.objects.count()
+
+
+    return render(request, 'index.html', {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_languages': num_languages,
+    })
