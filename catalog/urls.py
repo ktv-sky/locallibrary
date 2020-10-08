3842df7222f37e4019dec1bd04f1b177b6dfa874
ktv-sky <helicooper7@gmail.com>
2020-10-08 15:27:58 +0300

    Add permissions

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,10 +8,18 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(),
+        name='author-detail'),
 ]
 
 urlpatterns += [
-    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(),
+        name='my-borrowed'),
+    path('borrowed/', views.LoanedBooksAllListView.as_view(),
+        name='all-borrowed'),
+]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian,
+        name='renew-book-librarian'),
 ]
